ed7b8743d1e229575fdc18dcbd7381b3b5aad9c6
Azaken1248 <suvkrm@gmail.com>
2026-05-11 01:56:27 +0530

    update:(Rohit) update refrential integrity mappings for favourite conversions

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 import User from './User.js';
 import RefreshToken from './RefreshToken.js';
 import MeasurementHistory from './MeasurementHistory.js';
+import FavoriteConversion from './FavoriteConversion.js';
 
 User.hasMany(RefreshToken, { foreignKey: 'userId', as: 'refreshTokens' });
 RefreshToken.belongsTo(User, { foreignKey: 'userId', as: 'user' });
@@ -8,4 +9,7 @@ RefreshToken.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 User.hasMany(MeasurementHistory, { foreignKey: 'userId', as: 'measurementHistory' });
 MeasurementHistory.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-export { User, RefreshToken, MeasurementHistory };
+User.hasMany(FavoriteConversion, { foreignKey: 'userId', as: 'favoriteConversions' });
+FavoriteConversion.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+export { User, RefreshToken, MeasurementHistory, FavoriteConversion };
