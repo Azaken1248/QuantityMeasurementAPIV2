4d8c70a26c62c52bd4668a668cb2ccc7077fcf06
Azaken1248 <suvkrm@gmail.com>
2026-05-11 00:40:23 +0530

    update:(Rohit) update measurement router

--- src/routes/measurement.routes.js
+++ src/routes/measurement.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/auth.middleware.js';
+import measurementController from '../controllers/measurement.controller.js';
 
 const router = express.Router();
 
 router.use(verifyToken);
 
-// router.post('/convert', measurementController.convert);
+router.post('/convert', measurementController.convert);
 // router.post('/compare', measurementController.compare);
 // router.post('/calculate', measurementController.calculate);
 
