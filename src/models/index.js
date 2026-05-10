07faedaf661efb9696a1886b7afecfe8460e7457
Azaken1248 <suvkrm@gmail.com>
2026-05-11 00:38:07 +0530

    update:(Rohit) update referential integrity constraints

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,11 @@
 import User from './User.js';
 import RefreshToken from './RefreshToken.js';
+import MeasurementHistory from './MeasurementHistory.js';
 
 User.hasMany(RefreshToken, { foreignKey: 'userId', as: 'refreshTokens' });
 RefreshToken.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-export { User, RefreshToken };
+User.hasMany(MeasurementHistory, { foreignKey: 'userId', as: 'measurementHistory' });
+MeasurementHistory.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+export { User, RefreshToken, MeasurementHistory };
