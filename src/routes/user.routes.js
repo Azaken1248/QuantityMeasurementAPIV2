eaa7fdd659c7327215a08993c7746c04a7a20ff1
Azaken1248 <suvkrm@gmail.com>
2026-05-11 02:05:38 +0530

    update:(Rohit) update user router to include history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/auth.middleware.js';
 import favoriteController from '../controllers/favorite.controller.js';
+import historyController from '../controllers/history.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.use(verifyToken);
 router.post('/me/favorites', favoriteController.create);
 router.get('/me/favorites', favoriteController.list);
 router.delete('/me/favorites/:id', favoriteController.remove);
+
+router.get('/me/history', historyController.list);
 
 export default router;
