3d461079acecca9759bc53048602b101cfb2ff83
Azaken1248 <suvkrm@gmail.com>
2026-05-11 01:57:03 +0530

    update:(Rohit) update the master router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './auth.routes.js';
 import measurementRoutes from './measurement.routes.js';
+import userRoutes from './user.routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/measure', measurementRoutes);
+router.use('/users', userRoutes);
 
 export default router;
